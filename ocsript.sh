30019af89e2cc91380cc92e8020c16f347774ad2
nel003 <37954915+nel003@users.noreply.github.com>
2018-04-03 17:13:56 +0800

    Update ocsript.sh

--- ocsript.sh
+++ ocsript.sh
@@ -46,7 +46,7 @@ echo "First I need to know the new password of MySQL root user:"
 read -p "Password baru: " -e -i Nel003 DatabasePass
 echo ""
 echo "Finally, name the Database Name for OCS Panels"
-echo " Please, use one word only, no special characters other than Underscore (_)"
+echo " Please, use one word only, no special characters"
 read -p " Database Name: " -e -i OCS_PANEL DatabaseName
 echo ""
 echo "Okay, that's all I need. We are ready to setup your OCS Panels now"
