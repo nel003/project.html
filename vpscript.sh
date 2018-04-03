5fc7032ec397937ea39b6e36841ddf8da07dd08e
nel003 <37954915+nel003@users.noreply.github.com>
2018-04-04 07:22:09 +0800

    Update vpscript.sh

--- vpscript.sh
+++ vpscript.sh
@@ -170,7 +170,7 @@ cd ddos-deflate-master
 rm -rf /root/ddos-deflate-master.zip 
 
 # bannerrm /etc/issue.net
-wget -O /etc/issue.net "https://raw.githubusercontent.com/nel003/project.html/master/2nd.txt"
+wget -O /etc/issue.net "https://raw.githubusercontent.com/nel003/project.html/master/issue.net"
 sed -i 's@#Banner@Banner@g' /etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 service ssh restart
